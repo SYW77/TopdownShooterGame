daedaf0daa67526493ea8cbe7bf7139d8c7e7db0
SYW77 <son040912@naver.com>
2025-01-20 10:39:02 +0900

    25-01-20
    Create player character / Input and control / Collision handling / Running animation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3;
+    Vector3 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A))
+        {
+            move += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            move += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            move += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            move += new Vector3(0, -1, 0);
+        }
+
+        move = move.normalized;
+
+        if (move.x < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (move.x > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        if(move.magnitude > 0)
+        {
+            GetComponent<Animator>().SetTrigger("Move");
+        }
+        else
+        {
+            GetComponent<Animator>().SetTrigger("Stop");
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        transform.Translate(move * speed * Time.fixedDeltaTime);
+    }
+}
